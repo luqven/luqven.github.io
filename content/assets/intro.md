a760832448c0e45955a12c5d0987f95f286d9eb9
Luis H Ball Jr <luqven@gmail.com>
2020-06-08 20:17:49 -0700

    query for homepage md files

---
+++ content/assets/intro.md
@@ -0,0 +1,11 @@
+---
+title: About me
+date: "2015-05-01T22:14:03.284Z"
+description: A little about me
+---
+
+I love designing and programming user experiences. I went into software development because of the field’s combination of problem solving and design.
+
+I most recently helped the team at Parsec Media optimize and develop the Adelaide client-side javascript libraries which monitor a user's attention to ad units for hundreds of thousands of sessions a day.
+
+I graduated with dual-degree in Technical Writing & History from Carnegie Mellon University, after which I worked at an online publisher for 2 years.
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,27 +5,37 @@ import { PageProps, Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Card from "../components/card";
+import WorkExperience from "../components/experience"
 
 type Data = {
-    site: {
-        siteMetadata: {
-            title: string
-        }
+  site: {
+    siteMetadata: {
+      title: string
     }
+  }
+  allMarkdownRemark: {
+    edges: array
+  }
 }
 
 const HomePage = ({ data, location }: PageProps<Data>) => {
     const siteTitle = data.site.siteMetadata.title
+    const nodesArray = {...data.allMarkdownRemark.edges}
+    const aboutHtml = nodesArray[0].node.html;
 
+    console.log(aboutHtml)
     return (
         <Layout location={location} title={siteTitle}>
             <SEO title="Homepage" />
             <Bio />
             <h1> A little about me</h1>
-            <Card>
-              <p>Some card content to illustrate what this is for.</p>
-            </Card>
+            <p
+              dangerouslySetInnerHTML={{
+                __html: aboutHtml,
+              }}
+            />
+            <h1> Recent work</h1>
+            <WorkExperience />
         </Layout>
     )
 }
@@ -33,10 +43,19 @@ const HomePage = ({ data, location }: PageProps<Data>) => {
 export default HomePage
 
 export const pageQuery = graphql`
-  query {
+query {
     site {
       siteMetadata {
         title
+      }
+    }
+    allMarkdownRemark(
+      filter: { frontmatter: { title: { eq: "About me" } } }
+    ) {
+      edges {
+        node {
+          html
+        }
       }
     }
   }
